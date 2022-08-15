2503b7fa38dfd5e754f1d7593e232d7f84aa75f8
pangpang <1144269979@qq.com>
2022-08-15 14:15:54 +0800

    article-init

---
+++ src/components/ArticleHeader.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="article-header-container">
+    <div class="title">【Java基础篇】抽象类和接口概念和区别</div>
+    <div class="auth">
+      <div class="auth-left">
+        <div class="left">
+          <img src="@/assets/detail/avatar.png" alt="" />
+        </div>
+        <div class="right">
+          <div class="id">
+            <span>Java白羊</span>
+            <img src="@/assets/detail/level.png" alt="" title="创作等级" />
+          </div>
+          <div class="desc">
+            <div
+              class="date"
+              title="Fri Aug 12 2022 03:32:25 GMT+0800 (中国标准时间)"
+            >
+              2022年08月12日 03:32
+            </div>
+            <span>&nbsp;· 阅读</span>
+            <div class="nums">111</div>
+          </div>
+        </div>
+      </div>
+      <div class="auth-right unfocus" @click="focus = !focus" v-if="!focus">
+        +&nbsp;关注
+      </div>
+      <div class="auth-right focus" @click="focus = !focus" v-if="focus">
+        <span class="txt1"> √&nbsp;已关注</span>
+        <span class="txt2">√&nbsp;取消关注</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      focus: false,
+    };
+  },
+};
+</script>
+
+<style scoped lang="less">
+.article-header-container {
+    display: flex;
+    flex-direction: column;
+    row-gap: 25px;
+  .title {
+    font-size: 30px;
+    font-weight: bold;
+    color: #252933;
+  }
+  .auth {
+    display: flex;
+    justify-content: space-between;
+    .auth-left {
+      display: flex;
+      column-gap: 10px;
+      .left {
+        cursor: pointer;
+        img {
+          width: 40px;
+          height: 40px;
+          border-radius: 50%;
+        }
+      }
+      .right {
+        display: flex;
+        flex-direction: column;
+        row-gap: 5px;
+        .id {
+          display: flex;
+          column-gap: 5px;
+          cursor: pointer;
+          img {
+            width: 35px;
+            height: 16px;
+          }
+        }
+        .desc {
+          display: flex;
+          color: #8a919f;
+          font-size: 14px;
+        }
+      }
+    }
+    .auth-right {
+      cursor: pointer;
+      height: 32px;
+      line-height: 32px;
+      padding: 0 12px;
+      text-align: center;
+      font-size: 14px;
+      &.unfocus {
+        color: #1e80ff;
+        background: rgba(30, 128, 255, 0.05);
+        border: 1px solid rgba(30, 128, 255, 0.3);
+        border-radius: 4px;
+      }
+      &.focus {
+        color: #1e80ff;
+        background: rgba(30, 128, 255, 0.05);
+        border: 1px solid rgba(30, 128, 255, 0.3);
+        border-radius: 4px;
+        color: #515767;
+        background: rgba(81, 87, 103, 0.05);
+        border-color: rgba(81, 87, 103, 0.3);
+        .txt2 {
+          display: none;
+        }
+        &:hover {
+          .txt2 {
+            display: block;
+          }
+          .txt1 {
+            display: none;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,40 +1,76 @@
 <template>
-  <div>
-    <h1>this is article page</h1>
+  <div class="article-container">
+    <!-- 头部导航栏 -->
+    <div class="header">头部导航栏</div>
+    <!-- 侧边导航栏 -->
+    <div class="sidebar">侧边导航栏</div>
+    <!-- 文章详情主区域 -->
+    <div class="article-area">
+      <!-- 文章内容 -->
+      <article>
+        <!-- 标题 -->
+        <ArticleHeader />
+        <!-- 正文 -->
+        <div class="main-text">
+          <img src="@/assets/detail/test.png" alt="" />
+        </div>
+      </article>
+    </div>
   </div>
 </template>
 
 <script>
+import ArticleHeader from "@/components/ArticleHeader.vue";
 export default {
-  data() {
-    return {};
-  },
-  created() {
-    this.getCategory();
-    this.getComment();
-    this.getArticle();
-  },
-  methods: {
-    async getCategory() {
-      const res = await this.$api.getCategory();
-      console.log("类别", res);
-    },
-    async getComment() {
-      const res = await this.$api.getComment({
-        articleId: "6953423646664687652",
-        pageIndex: "1",
-      });
-      console.log("comment", res);
-    },
-    async getArticle() {
-      const res = await this.$api.getArticle({
-        cateId: "6809637767543259144",
-        pageIndex: "0",
-      });
-      console.log("article", res);
-    },
+  components: {
+    ArticleHeader,
   },
 };
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.article-container {
+  background-color: #f4f5f5;
+  width: 100%;
+  padding-bottom: 30px;
+  // 头部导航栏
+  .header {
+    height: 5rem;
+    line-height: 5rem;
+    background-color: lightgray;
+    text-align: center;
+    color: #fff;
+  }
+  // 侧边导航栏
+  .sidebar {
+    position: fixed;
+    top: 100px;
+    right: 380px;
+    width: 300px;
+    height: 600px;
+    line-height: 600px;
+    background-color: lightgray;
+    text-align: center;
+    color: #fff;
+  }
+  // 文章详情主区域
+  .article-area {
+    box-sizing: border-box;
+    background-color: #fff;
+    width: 820px;
+    padding: 32px 32px 39px;
+    margin: 20px 700px 0 auto;
+    article {
+      display: flex;
+      flex-direction: column;
+      row-gap: 25px;
+      .main-text {
+        img {
+          width: 100%;
+          height: 425px;
+        }
+      }
+    }
+  }
+}
+</style>
